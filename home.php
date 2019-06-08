10c63b6dd2a5d86882c84d8cc37b34edd8f762f1
Zenoo <fzenooo@gmail.com>
2019-06-08 18:02:16 +0200

    added webp images

--- home.php
+++ home.php
@@ -83,7 +83,11 @@
 				<a class="download" href="https://stackoverflow.com/story/zenoo" target="_blank"
 					title="Developer Story"><i class="fa fa-cloud-download"></i></a>
 				<div class="img-wrap">
-					<img src="images/photo.png" alt="Franck Demoute" width="150" height="150" />
+					<picture>
+						<source srcset="images/webp/photo.webp" type="image/webp" width="150" height="150">
+						<source srcset="images/photo.png" type="image/png" width="150" height="150">
+						<img src="images/photo.png" alt="Franck Demoute" width="150" height="150">
+					</picture>
 				</div>
 				<div class="profile_info">
 					<h1>Franck Demoute</h1>
@@ -173,8 +177,12 @@
 								</div>
 								<div class="face1 card-face">
 									<div class="about-cover card-face">
-										<a class="map-location" data-card-front="data-card-front"><img
-												src="images/map-icon.png" alt="Je suis ici !">
+										<a class="map-location" data-card-front="data-card-front">
+											<picture>
+												<source srcset="images/webp/map-icon.webp" type="image/webp">
+												<source srcset="images/map-icon.png" type="image/png">
+												<img src="images/map-icon.png" alt="Je suis ici !">
+											</picture>
 										</a>
 										<div class="about-details">
 											<div><span class="fa fa-inbox"></span><span
@@ -684,7 +692,11 @@
 						<div class="portfolio">
 							<!-- Portfolio Item -->
 							<figure class="effect-milo">
-								<img src="images/realisations/biard-demenagements.fr.png" alt="Site de Biard Déménagements" width="395" height="222" />
+								<picture>
+									<source srcset="images/webp/realisations/biard-demenagements.fr.webp" type="image/webp" width="395" height="222">
+									<source srcset="images/realisations/biard-demenagements.fr.png" type="image/png" width="395" height="222">
+									<img src="images/realisations/biard-demenagements.fr.png" alt="Site de Biard Déménagements" width="395" height="222">
+								</picture>
 								<figcaption>
 									<span class="label">Design Integration</span>
 									<div class="portfolio_button">
@@ -696,8 +708,11 @@
 									<div class="mfp-hide mfp-with-anim work_desc work1">
 										<div class="col-md-6">
 											<div class="image_work">
-												<img src="images/realisations/biard-demenagements.fr-big.png" alt="Site de Biard Déménagements" width="747"
-													height="420">
+												<picture>
+													<source srcset="images/webp/realisations/biard-demenagements.fr-big.webp" type="image/webp" width="747" height="420">
+													<source srcset="images/realisations/biard-demenagements.fr-big.png" type="image/png" width="747" height="420">
+													<img src="images/realisations/biard-demenagements.fr-big.png" alt="Site de Biard Déménagements" width="747" height="420">
+												</picture>
 											</div>
 										</div>
 										<div class="col-md-6">
@@ -723,7 +738,11 @@
 
 							<!-- Portfolio Item -->
 							<figure class="effect-milo">
-								<img src="images/realisations/quizzconex.fr.png" alt="Site de QuizConex" width="395" height="222" />
+								<picture>
+									<source srcset="images/webp/realisations/quizzconex.fr.webp" type="image/webp" width="395" height="222">
+									<source srcset="images/realisations/quizzconex.fr.png" type="image/png" width="395" height="222">
+									<img src="images/realisations/quizzconex.fr.png" alt="Site de QuizConex" width="395" height="222">
+								</picture>
 								<figcaption>
 									<span class="label">Web Application</span>
 									<div class="portfolio_button">
@@ -735,8 +754,11 @@
 									<div class="mfp-hide mfp-with-anim work_desc work2">
 										<div class="col-md-6">
 											<div class="image_work">
-												<img src="images/realisations/quizzconex.fr-big.png" alt="Site de QuizConex" width="747"
-													height="420">
+												<picture>
+													<source srcset="images/webp/realisations/quizzconex.fr-big.webp" type="image/webp" width="747" height="420">
+													<source srcset="images/realisations/quizzconex.fr-big.png" type="image/png" width="747" height="420">
+													<img src="images/realisations/quizzconex.fr-big.png" alt="Site de QuizConex" width="747" height="420">
+												</picture>
 											</div>
 										</div>
 										<div class="col-md-6">
@@ -761,7 +783,11 @@
 
 							<!-- Portfolio Item -->
 							<figure class="effect-milo">
-								<img src="images/realisations/obcom.fr.png" alt="Site de la société Obcom" width="395" height="222" />
+								<picture>
+									<source srcset="images/webp/realisations/obcom.fr.webp" type="image/webp" width="395" height="222">
+									<source srcset="images/realisations/obcom.fr.png" type="image/png" width="395" height="222">
+									<img src="images/realisations/obcom.fr.png" alt="Site d'Obcom'" width="395" height="222">
+								</picture>
 								<figcaption>
 									<span class="label">Website</span>
 									<div class="portfolio_button">
@@ -773,8 +799,11 @@
 									<div class="mfp-hide mfp-with-anim work_desc work3">
 										<div class="col-md-6">
 											<div class="image_work">
-												<img src="images/realisations/obcom.fr-big.png" alt="Site de la société Obcom" width="747"
-													height="420">
+												<picture>
+													<source srcset="images/webp/realisations/obcom.fr-big.webp" type="image/webp" width="747" height="420">
+													<source srcset="images/realisations/obcom.fr-big.png" type="image/png" width="747" height="420">
+													<img src="images/realisations/obcom.fr-big.png" alt="Site d'Obcom" width="747" height="420">
+												</picture>
 											</div>
 										</div>
 										<div class="col-md-6">
@@ -799,7 +828,11 @@
 
 							<!-- Portfolio Item -->
 							<figure class="effect-milo">
-								<img src="images/realisations/somovers.com.png" alt="Site de Somovers" width="395" height="222" />
+								<picture>
+									<source srcset="images/webp/realisations/somovers.com.webp" type="image/webp" width="395" height="222">
+									<source srcset="images/realisations/somovers.com.png" type="image/png" width="395" height="222">
+									<img src="images/realisations/somovers.com.png" alt="Site de Somovers" width="395" height="222">
+								</picture>
 								<figcaption>
 									<span class="label">Website</span>
 									<div class="portfolio_button">
@@ -811,8 +844,11 @@
 									<div class="mfp-hide mfp-with-anim work_desc work4">
 										<div class="col-md-6">
 											<div class="image_work">
-												<img src="images/realisations/somovers.com-big.png" alt="Site de Somovers" width="747"
-													height="420">
+												<picture>
+													<source srcset="images/webp/realisations/somovers.com-big.webp" type="image/webp" width="747" height="420">
+													<source srcset="images/realisations/somovers.com-big.png" type="image/png" width="747" height="420">
+													<img src="images/realisations/somovers.com-big.png" alt="Site de Somovers" width="747" height="420">
+												</picture>
 											</div>
 										</div>
 										<div class="col-md-6">
@@ -835,7 +871,11 @@
 
 							<!-- Portfolio Item -->
 							<figure class="effect-milo">
-								<img src="images/realisations/clos-normand.com.png" alt="Site du Clos Normand" width="395" height="222" />
+								<picture>
+									<source srcset="images/webp/realisations/clos-normand.com.webp" type="image/webp" width="395" height="222">
+									<source srcset="images/realisations/clos-normand.com.png" type="image/png" width="395" height="222">
+									<img src="images/realisations/clos-normand.com.png" alt="Site du Clos Normand" width="395" height="222">
+								</picture>
 								<figcaption>
 									<span class="label">Online Store</span>
 									<div class="portfolio_button">
@@ -847,8 +887,11 @@
 									<div class="mfp-hide mfp-with-anim work_desc work5">
 										<div class="col-md-6">
 											<div class="image_work">
-												<img src="images/realisations/clos-normand.com-big.png" alt="Site du Clos Normand" width="747"
-													height="420">
+												<picture>
+													<source srcset="images/webp/realisations/clos-normand.com-big.webp" type="image/webp" width="747" height="420">
+													<source srcset="images/realisations/clos-normand.com-big.png" type="image/png" width="747" height="420">
+													<img src="images/realisations/clos-normand.com-big.png" alt="Site du Clos Normand" width="747" height="420">
+												</picture>
 											</div>
 										</div>
 										<div class="col-md-6">
@@ -872,7 +915,11 @@
 
 							<!-- Portfolio Item -->
 							<figure class="effect-milo">
-								<img src="images/realisations/atlantis.png" alt="Site de la société Atlantis Eco BTP" width="395" height="222" />
+								<picture>
+									<source srcset="images/webp/realisations/atlantis.webp" type="image/webp" width="395" height="222">
+									<source srcset="images/realisations/atlantis.png" type="image/png" width="395" height="222">
+									<img src="images/realisations/atlantis.png" alt="Site de la société Atlantis Eco BTP" width="395" height="222">
+								</picture>
 								<figcaption>
 									<span class="label">Website</span>
 									<div class="portfolio_button">
@@ -884,8 +931,11 @@
 									<div class="mfp-hide mfp-with-anim work_desc work6">
 										<div class="col-md-6">
 											<div class="image_work">
-												<img src="images/realisations/atlantis-big.png" alt="Site de la société Atlantis Eco BTP" width="747"
-													height="420">
+												<picture>
+													<source srcset="images/webp/realisations/atlantis-big.webp" type="image/webp" width="747" height="420">
+													<source srcset="images/realisations/atlantis-big.png" type="image/png" width="747" height="420">
+													<img src="images/realisations/atlantis-big.png" alt="Site de la société Atlantis Eco BTP" width="747" height="420">
+												</picture>
 											</div>
 										</div>
 										<div class="col-md-6">
