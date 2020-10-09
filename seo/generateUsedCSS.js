275e712328dbb350020509becc80f78d267da373
Zenoo <perso@franckdemoute.fr>
2020-10-09 14:55:47 +0200

    fix: Minify CSS + only load critical at first

---
+++ seo/generateUsedCSS.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('Coverage.json');
+let json = JSON.parse(rawdata);
+
+
+const style = json.find(o => o.url == 'https://franckdemoute.fr/style.css');
+
+const usedCSS = style.ranges.map(range => style.text.substring(range.start, range.end)).join('');
+
+fs.writeFileSync('seo/usedCSS.css', usedCSS);
