af20ba7cda1219e98c284e06d8c5a6ddfa2618eb
Franck <perso@franckdemoute.fr>
2025-06-02 09:08:54 +0200

    fix: Add new position

--- utils/data/experience.ts
+++ utils/data/experience.ts
@@ -1,18 +1,24 @@
 export const experiences = [
   {
     id: 1,
-    title: 'FullStack Developer',
-    company: "Sostag / Obcom",
-    duration: "(2017 - Present)"
+    title: 'IT Designer Developer',
+    company: "Business Tax",
+    duration: "(2025 - Present)"
   },
   {
     id: 2,
+    title: 'FullStack Developer',
+    company: "Sostag / Obcom",
+    duration: "(2017 - 2025)"
+  },
+  {
+    id: 3,
     title: "External Applications Community Developer",
     company: "MotionTwin",
     duration: "(2012 - 2018)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Developer intern",
     company: "Obcom / R2Design",
     duration: "(2017)"
